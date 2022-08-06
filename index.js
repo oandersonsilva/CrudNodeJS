ca9b233dd4c87479c3e66e17359944fe60718477
oandersonsilva <oandersonsouza.dev@gmail.com>
2022-08-05 21:24:41 -0300

    Database Connection

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser') //trabalha com os dados vindos dos clientes raves do form e os transforma em objeto JS dentro do Req.body
+const Sequelize = require('sequelize')
+const connection = require('./database/database')
+const model = require('./database/model1')
+
+app.use(bodyParser.urlencoded({ extended: false })) //evita que utilizem campos encadeados
+app.use(bodyParser.json())
 
 port = 3000
 app.use(express.static('public'))
@@ -10,6 +17,17 @@ app.get('/', (req, res) => {
 
 app.get('/cadastro', (req, res) => {
   res.render('cadastro')
+})
+
+app.post('/pessoaCadastrada', (req, res) => {
+  var nome = req.body.Inome
+  var email = req.body.Iemail
+  console.log('Cadastro efetuado com sucesso')
+  model.create({
+    nome: nome,
+    email: email
+  })
+  res.send(nome)
 })
 
 app.listen(port)
