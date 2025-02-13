e9df0c1a06d34b0b8a14532be72aab56637e2577
oandersonsilva <oandersonsouza.dev@gmail.com>
2025-02-13 20:44:36 -0300

    Atualizando rotas
    Separando as rotas das tabelas Unidades e Produtos

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,9 +1,150 @@
-function rota(application, loginAuth, tableProdutos){
+function rota(application, loginAuth, tableProdutos) {
 
+    // consultar todos
 
+    application.get('/produtos/consultaAll', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
 
+        tableProdutos
+            .findAll({raw: true, order: ['valor']})
+            .then(vetor => {
+                res.render('../views/produtos/consultaAll', {
+                    item: vetor,
+                    UsernamePag: username
+                })
+            })
+    })
 
+    //Cadastro de Produtos
 
+    application.post('/cadastroProdutos', (req, res) => {
+        const descricao = req.body.Idescricao
+        const valor = req
+            .body
+            .Ivalor
+
+            tebleProdutos
+            .create({descricao: descricao, valor: valor})
+        console.log('cadastrado com sucesso')
+        res.redirect('/')
+    })
+
+    // Abrir pagina cadastro de produtos
+
+    application.get('/produtos/cadastro', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+        res.render('../views/produtos/cadastro', {UsernamePag: username})
+    })
+
+    // Abrir pagina de consulta de produtos
+
+    application.get('/produtos/consulta', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+
+        var item = []
+        res.render('../views/produtos/consulta', {
+            item: item,
+            UsernamePag: username
+        })
+    })
+
+    application.post('/produtos/consulta', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+
+        var descricao = req
+            .body
+            .descricao
+            tableProdutos
+            .findOne({
+                where: {
+                    descricao: descricao
+                }
+            })
+            .then(item => {
+                if (item != undefined) {
+                    res.render('../views/produtos/consulta', {
+                        item: item,
+                        UsernamePag: username
+                    })
+                } else {}
+            })
+    })
+
+    // Atualização de produtos
+
+    application.post('/updateProdutos', (req, res) => {
+        var id = req.body.id
+        var descricao = req.body.Idescricao
+        var valor = req
+            .body
+            .Ivalor
+            console
+            .log(`${id}, ${descricao}, e ${valor} `)
+        tableProdutos
+            .update({
+                descricao: descricao,
+                valor: valor
+            }, {
+                where: {
+                    id: id
+                }
+            })
+            .then(() => {
+                res.redirect('/produtos/consultaAll')
+            })
+    })
+
+    // Deletando produtos
+
+    application.post('/deletarProdutos', (req, res) => {
+        var id = req
+            .body
+            .id
+            tableProdutos
+            .destroy({
+                where: {
+                    id: id
+                }
+            })
+            .then(() => {
+                res.redirect('/produtos/consultaAll')
+            })
+    })
+
+    application.get('/produtos/editar/:id', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+
+        var id = req
+            .params
+            .id
+            tableProdutos
+            .findOne({
+                where: {
+                    id: id
+                }
+            })
+            .then(item => {
+                res.render('../views/produtos/editar', {
+                    item: item,
+                    UsernamePag: username
+                })
+            })
+    })
 
 }
 
--- routes/unidades.js
+++ routes/unidades.js
@@ -1,9 +1,100 @@
-function rota(application, loginAuth, tableUnidades){
+function rota(application, loginAuth, tableUnidades) {
 
+    //Unidades
 
+    application.get('/unidades/consulta', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
 
+        tableUnidades
+            .findAll()
+            .then(data => {
+                res.render('../views/unidades/consulta', {
+                    UsernamePag: username,
+                    items: data
+                })
+            })
+    })
 
+    application.get('/unidades/cadastro', (req, res) => {
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+        res.render('../views/unidades/cadastro', {UsernamePag: username})
+    })
 
+    // cadastro Unidade
+
+    application.post('/cadastrarUnidade', (req, res) => {
+        const local = req.body.Ilocal
+        const numFunc = req
+            .body
+            .InumFunc
+
+            tableUnidades
+            .create({local: local, numeroFuncionarios: numFunc})
+        res.redirect('/')
+    })
+
+    // editar Unidade
+    application.get('/unidades/editar/:id', (req, res) => {
+        var id = req.params.id
+        var username = ''
+        if (req.session.nome) {
+            username = req.session.nome
+        }
+        tableUnidades
+            .findOne({
+                where: {
+                    id: id
+                }
+            })
+            .then(data => {
+                res.render('../views/unidades/editar', {
+                    UsernamePag: username,
+                    item: data
+                })
+            })
+    })
+
+    application.post('/editarUnidade', (req, res) => {
+        var local = req.body.Ilocal
+        var NFunc = req.body.InumFunc
+        var id = req
+            .body
+            .Iid
+            tableUnidades
+            .update({
+                local: local,
+                numeroFuncionarios: NFunc
+            }, {
+                where: {
+                    id: id
+                }
+            })
+            .then(res.redirect('/'))
+    })
+
+    //deletar unidade
+
+    application.get('/unidades/deletarUnidade/:id', (req, res) => {
+        var id = req
+            .params
+            .id
+            tableUnidades
+            .destroy({
+                where: {
+                    id: id
+                }
+            })
+            .then(res.redirect('/'))
+            .catch(err => {
+                console.log(err)
+            })
+        })
 
 }
 
